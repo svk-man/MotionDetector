1bfca66758a745bb351b46d0e7d958ae25412dce
svk-man <kravchenko.sgv@gmail.com>
2019-12-22 23:29:18 +0400

    Поиск контуров

--- main.py
+++ main.py
@@ -25,11 +25,23 @@ while True:
     cv2.imshow('frame-delta', delta_frame)
 
     # Преобразовать кадр в оттенках серого в черно-белый
-    thresh_delta = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_delta = cv2.threshold(delta_frame, 20, 255, cv2.THRESH_BINARY)[1]
 
     # Расширить светлые области и сузить темные
-    thresh_delta = cv2.dilate(delta_frame, None, iterations=0)
+    thresh_delta = cv2.dilate(thresh_delta, None, iterations=2)
     cv2.imshow('frame-dilate', thresh_delta)
+
+    # Поиск контуров
+    (cnts, _) = cv2.findContours(thresh_delta.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv2.contourArea(contour) < 1000:
+            continue
+
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    cv2.imshow('frame-result', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
