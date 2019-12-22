dd92c75b67149684b3490572631b1a60d4a628aa
svk-man <kravchenko.sgv@gmail.com>
2019-12-22 21:39:09 +0400

    Сохранить первый кадр видео

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 
+first_frame = None
+
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -12,6 +14,11 @@ while True:
     # Размытие фильтром Гаусса
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
     cv2.imshow('frame-blur', gray)
+
+    # Сохранить первый кадр видео
+    if first_frame is None:
+        first_frame = gray
+        continue
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
