12acc1f989392da2cc32a0554ac943a69b498634
svk-man <kravchenko.sgv@gmail.com>
2019-12-22 21:37:05 +0400

    Размытие изображения с веб-камеры фильтром Гаусса

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ while True:
     ret, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     cv2.imshow('frame', gray)
+
+    # Размытие фильтром Гаусса
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    cv2.imshow('frame-blur', gray)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
