b57c02325e4bee9e8f989da4947cb25bdceeb4af
svk-man <kravchenko.sgv@gmail.com>
2020-01-24 00:04:25 +0400

    mark: Рисование прямоугольника (update)

--- mark.py
+++ mark.py
@@ -53,30 +53,31 @@ jx, jy = -1, -1
 
 
 def draw_rect(event, x, y, flags, param):
-    global drawing, ix, iy, jx, jy
+    global drawing, ix, iy, jx, jy, clone_image
     jx, jy = x, y
 
     if event == cv2.EVENT_LBUTTONDOWN:
         drawing = True
         ix, iy = x, y
 
+    if event == cv2.EVENT_MOUSEMOVE:
+        if drawing:
+            clone_image = image.copy()
+            cv2.rectangle(clone_image, (ix, iy), (jx, jy), (0, 255, 0), 2)
+
     if event == cv2.EVENT_LBUTTONUP:
         drawing = False
+        clone_image = image.copy()
         cv2.rectangle(clone_image, (ix, iy), (x, y), (0, 255, 0), 2)
 
 
 image = cv2.imread('temp/video#1.mp4/frame1824.png')
 cv2.namedWindow('image')
-cv2.setMouseCallback('image', draw_rect)
 clone_image = image.copy()
+cv2.setMouseCallback('image', draw_rect)
 
 while 1:
-    if drawing:
-        cv2.rectangle(clone_image, (ix, iy), (jx, jy), (0, 255, 0), 2)
-
     cv2.imshow("image", clone_image)
-    if drawing:
-        clone_image = image.copy()
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
