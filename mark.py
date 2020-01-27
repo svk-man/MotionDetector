f92ac835f97e51b8d8a9f9d54d7e9d512ef91acd
svk-man <kravchenko.sgv@gmail.com>
2020-01-27 20:55:00 +0400

    mark: Покадровый (переход с помощью кнопки "space") просмотр видео

--- mark.py
+++ mark.py
@@ -49,7 +49,7 @@ print(n)'''
 
 
 def draw_rect(event, x, y, flags, param):
-    global drawing, draging, ix, iy, jx, jy, dx1, dy1, dx2, dy2, image, clone_image, rect
+    global drawing, draging, ix, iy, jx, jy, dx1, dy1, dx2, dy2, frame, clone_frame, rect
     jx, jy = x, y
 
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -63,21 +63,21 @@ def draw_rect(event, x, y, flags, param):
 
     if event == cv2.EVENT_MOUSEMOVE:
         if drawing:
-            clone_image = image.copy()
+            clone_frame = frame.copy()
             if not draging:
-                cv2.rectangle(clone_image, (ix, iy), (jx, jy), (0, 255, 0), 2)
+                cv2.rectangle(clone_frame, (ix, iy), (jx, jy), (0, 255, 0), 2)
             else:
-                cv2.rectangle(clone_image, (rect[0] + x - dx1, rect[1] + y - dy1),
+                cv2.rectangle(clone_frame, (rect[0] + x - dx1, rect[1] + y - dy1),
                               (rect[2] + x - dx1, rect[3] + y - dy1), (0, 255, 0), 2)
 
     if event == cv2.EVENT_LBUTTONUP:
         if ix != -1 and iy != -1:
-            clone_image = image.copy()
+            clone_frame = frame.copy()
             if not draging:
-                cv2.rectangle(clone_image, (ix, iy), (jx, jy), (0, 255, 0), 2)
+                cv2.rectangle(clone_frame, (ix, iy), (jx, jy), (0, 255, 0), 2)
                 rect = (ix, iy, jx, jy)
             else:
-                cv2.rectangle(clone_image, (rect[0] + x - dx1, rect[1] + y - dy1),
+                cv2.rectangle(clone_frame, (rect[0] + x - dx1, rect[1] + y - dy1),
                               (rect[2] + x - dx1, rect[3] + y - dy1),
                               (0, 255, 0), 2)
                 rect = (rect[0] + x - dx1, rect[1] + y - dy1, rect[2] + x - dx1, rect[3] + y - dy1)
@@ -92,27 +92,44 @@ jx, jy = -1, -1
 dx1, dy1, dx2, dy2 = -1, -1, -1, -1
 rect = (-1, -1, -1, -1)
 
-image = cv2.imread('temp/video#1.mp4/frame1824.png')
-cv2.namedWindow('image')
-clone_image = image.copy()
-cv2.setMouseCallback('image', draw_rect)
+cv2.namedWindow('frame')
 
-while 1:
-    cv2.imshow("image", clone_image)
+frame_size = 720
+frame_height = 480
 
-    key = cv2.waitKey(1)
-    if key & 0xFF == ord('q'):
+videoPath = 'video/video#1.mp4'
+cap = cv2.VideoCapture(videoPath)
+
+is_quit = False
+while cap.isOpened():
+    ret, frame = cap.read()
+    if frame is not None:
+        frame = cv2.resize(frame, (frame_size, frame_height))
+
+        clone_frame = frame.copy()
+        cv2.setMouseCallback('frame', draw_rect)
+
+        while 1:
+            cv2.imshow('frame', clone_frame)
+
+            key = cv2.waitKey(1)
+            if key & 0xFF == ord('q'):
+                is_quit = True
+                break
+            elif key == 32:                         # Нажата клавиша "space"
+                break
+            elif key == 27:                         # Нажата клавиша "ESC"
+                clone_frame = frame.copy()
+                cv2.imshow('frame', clone_frame)
+                drawing = False
+                draging = False
+                ix, iy = -1, -1
+                jx, jy = -1, -1
+                dx1, dy1, dx2, dy2 = -1, -1, -1, -1
+                rect = (-1, -1, -1, -1)
+
+    if is_quit:
         break
-    elif key == 27:                         # Нажата клавиша "ESC"
-        clone_image = image.copy()
-        cv2.imshow("image", clone_image)
-        drawing = False
-        draging = False
-        ix, iy = -1, -1
-        jx, jy = -1, -1
-        dx1, dy1, dx2, dy2 = -1, -1, -1, -1
-        rect = (-1, -1, -1, -1)
 
-
-cv2.waitKey(0)
+cap.release()
 cv2.destroyAllWindows()
