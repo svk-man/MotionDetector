f1b0aa06cc7e3004aa48d68f03728a1dcb9369f7
svk-man <kravchenko.sgv@gmail.com>
2020-03-16 02:00:06 +0400

    split: для всех видеофайлов в директории

--- split.py
+++ split.py
@@ -3,12 +3,16 @@ import glob
 import os
 import sys
 import cv2
+from os.path import join
 
 
 def validate(video_path, n):
     message = ''
     if not os.path.isfile(video_path):
-        message = 'Указанный путь к видео не существует'
+        if not os.path.isdir(video_path):
+            message = 'Указанная директория с видеофайлами не существует'
+        else:
+            message = 'Указанный путь к видео не существует'
 
     if n <= 0:
         message = 'N не может быть отрицательным числом'
@@ -53,15 +57,15 @@ def split_video(video_path, n):
     # Создать директорию с кадрами для заданного видео
     video_base_name = os.path.basename(video_path)
     video_name = os.path.splitext(video_base_name)[0]
-    video_dir = video_name + "/"
-    images_dir = "images/"
-    video_images_dir = video_dir + images_dir
+    video_dir = join(os.path.dirname(video_path), video_name)
+    images_dir = "images"
+    video_images_dir = join(video_dir, images_dir)
 
     if not os.path.exists(video_images_dir):
         os.makedirs(video_images_dir)
     else:
         # Удалить все кадры из целевой директории
-        images = glob.glob(video_images_dir + "*")
+        images = glob.glob(join(video_images_dir, "*"))
         for f in images:
             if not os.path.isdir(f):
                 os.remove(f)
@@ -70,18 +74,42 @@ def split_video(video_path, n):
     cap = cv2.VideoCapture(video_path)
 
     frame_id = 0
+    cur_dir = os.getcwd()
+    os.chdir(video_images_dir)
     while cap.isOpened():
         ret, frame = cap.read()
         if frame is not None:
             frame_id += 1
             if not (frame_id % n):
                 frame_name = 'image' + str(frame_id) + '.jpg'
-                cv2.imwrite(video_images_dir + frame_name, frame)
+                cv2.imwrite(frame_name, frame)
         else:
             break
 
+    os.chdir(cur_dir)
     cap.release()
     cv2.destroyAllWindows()
 
 
-split_video(video_path, n)
+if os.path.isdir(video_path):
+    # Получить все видеофайлы в директории
+    videos = []
+    files = []
+    for ext in ('*.mp4', '*.avi'):
+        files.extend(glob.glob(join(video_path, ext)))
+
+    for f in files:
+        if not os.path.isdir(f):
+            video_base_name = os.path.basename(f)
+            videos.append(video_base_name)
+            print(video_base_name)
+
+    print('Количество видеофайлов: ' + str(len(videos)))
+
+    for video in videos:
+        split_video(join(video_path, video), n)
+        print(video + ": завершено.")
+
+    print("Завершено.")
+else:
+    split_video(video_path, n)
