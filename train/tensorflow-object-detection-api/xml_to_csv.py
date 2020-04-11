0384c02772d7b945091f985690ce0fd0d7359275
svk-man <kravchenko.sgv@gmail.com>
2020-04-11 14:32:39 +0400

    tensorflow: xml to csv convertation

--- train/tensorflow-object-detection-api/xml_to_csv.py
+++ train/tensorflow-object-detection-api/xml_to_csv.py
@@ -2,13 +2,14 @@ import os
 import glob
 import pandas as pd
 import xml.etree.ElementTree as ET
-def xml_to_csv(path):
+def xml_to_csv(path, type_directory):
     xml_list = []
+    path_core = path[path.find("data/" + type_directory):None] + '/images/'
     for xml_file in glob.glob(path + '/*.xml'):
         tree = ET.parse(xml_file)
         root = tree.getroot()
         for member in root.findall('object'):
-            value = (root.find('filename').text,
+            value = (path_core + root.find('filename').text,
                      int(root.find('size')[0].text),
                      int(root.find('size')[1].text),
                      member[0].text,
@@ -21,11 +22,33 @@ def xml_to_csv(path):
     column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
     xml_df = pd.DataFrame(xml_list, columns=column_name)
     return xml_df
+
+# https://stackoverflow.com/questions/17530542/how-to-add-pandas-data-to-an-existing-csv-file
+def save_sml_to_csv(df, csv_path, sep=","):
+    if not os.path.isfile(csv_path):
+        df.to_csv(csv_path, mode='a', index=False, sep=sep)
+    elif len(df.columns) != len(pd.read_csv(csv_path, nrows=1, sep=sep).columns):
+        raise Exception("Columns do not match!! Dataframe has " + str(len(df.columns)) + " columns. CSV file has " + str(len(pd.read_csv(csv_path, nrows=1, sep=sep).columns)) + " columns.")
+    elif not (df.columns == pd.read_csv(csv_path, nrows=1, sep=sep).columns).all():
+        raise Exception("Columns and column order of dataframe and csv file do not match!!")
+    else:
+        df.to_csv(csv_path, mode='a', index=False, sep=sep, header=False)
+
+
 def main(directory_list):
     for Image_cat in directory_list:
-        image_path = os.path.join(os.getcwd(), 'images_test_1/{}'.format(Image_cat))
-        xml_df = xml_to_csv(image_path) 
-        xml_df.to_csv('data/{}_labels.csv'.format(Image_cat), index=None)
+        image_path = os.path.join(os.getcwd(), 'data/{}'.format(Image_cat))
+        print(image_path)
+        for i in os.walk(image_path):
+            if i[2]:
+                print("Folder: ")
+                print(i[0])
+                print("with images:")
+                print(i[2])
+                print()
+                xml_df = xml_to_csv(i[0], Image_cat)
+                save_sml_to_csv(xml_df, 'data/{}_labels.csv'.format(Image_cat))
+
         print('Successfully converted xml to csv.')
 
 main(['train','test'])
